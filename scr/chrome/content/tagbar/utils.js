31cb9aa0cc3a80d1c50637f7f5db638d28595952
dugite-code <dugite-code@peekread.info>
2018-08-23 12:04:14 +0800

    Replace getComplexValue with getStringPref

--- scr/chrome/content/tagbar/utils.js
+++ scr/chrome/content/tagbar/utils.js
@@ -1,7 +1,7 @@
 var gTTBPreferences = {
 	pref: Components.classes["@mozilla.org/preferences-service;1"].
 	  getService(Components.interfaces.nsIPrefBranch),
-	  
+
 	getIntPref: function(str, def) {
 		try {
 			return this.pref.getIntPref(str);
@@ -9,7 +9,7 @@ var gTTBPreferences = {
 			return def;
 		}
 	},
-	
+
 	getBoolPref: function(str, def) {
 		try {
 			return this.pref.getBoolPref(str);
@@ -17,27 +17,35 @@ var gTTBPreferences = {
 			return def;
 		}
 	},
-	
+
 	copyUnicharPref: function(str, def) {
 		try{
-			return this.pref.getComplexValue(str, Components.interfaces.nsISupportsString).data;
+			if ( Services.vc.compare(Services.appinfo.platformVersion, '58') < 0 ) {
+				return this.pref.getComplexValue(str, Components.interfaces.nsISupportsString).data;
+			} else {
+				return this.pref.getStringPref(str)
+			}
 		}catch(e){
 			return def;
 		}
 	},
-	
+
 	setIntPref: function(str, val) {
 		return this.pref.setIntPref(str, val);
 	},
-	
+
 	setBoolPref: function(str, val) {
 		return this.pref.setBoolPref(str, val);
 	},
-	
+
 	setUnicharPref: function(str, val) {
 		var sstr = Components.classes["@mozilla.org/supports-string;1"].
 							createInstance(Components.interfaces.nsISupportsString);
 		sstr.data = val;
-		this.pref.setComplexValue(str, Components.interfaces.nsISupportsString, sstr);
+		if ( Services.vc.compare(Services.appinfo.platformVersion, '58') < 0 ) {
+			this.pref.setComplexValue(str, Components.interfaces.nsISupportsString, sstr);
+		} else {
+			this.pref.setStringPref(str, sstr.data);
+		}
 	}
 }
